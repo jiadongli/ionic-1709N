592df0a4f3cd6ea33bf82340952ac366ad689529
thu <mingfei.net@gmail.com>
2018-02-07 11:39:15 +0800

    Update

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -3,6 +3,7 @@ import {NavController} from 'ionic-angular';
 import {ButtonPage} from '../button/button';
 import {ListPage} from '../list/list';
 import {CardPage} from '../card/card'
+import {FabPage} from '../fab/fab'
 
 @Component({
   selector: 'page-home',
@@ -13,11 +14,13 @@ export class HomePage {
   buttonPage;
   listPage;
   cardPage;
+  fabPage;
 
   constructor(public navCtrl: NavController) {
     this.buttonPage = ButtonPage;
     this.listPage = ListPage;
     this.cardPage = CardPage;
+    this.fabPage = FabPage;
   }
 
 }
