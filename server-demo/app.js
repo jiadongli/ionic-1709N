67555f71d7219883efca501bdca1fa6820a85e7c
thu <mingfei.net@gmail.com>
2018-02-08 15:37:44 +0800

    Update

--- server-demo/app.js
+++ server-demo/app.js
@@ -6,6 +6,15 @@ const express = require('express');
 
 let app = new express();
 
+app.get('/', (req, res) => {
+    // req = request
+    // res = response
+    // ...
+    res.end('It works.')
+});
+// 15:35
 app.listen(3000);
 
 // Alt + Shift + F10
+
+// E_ADDR_IN_USE:::3000
