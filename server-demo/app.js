d24c469893c82d12d7b5c9b08485ad2abca2d588
thu <mingfei.net@gmail.com>
2018-02-09 10:44:31 +0800

    Update

--- server-demo/app.js
+++ server-demo/app.js
@@ -3,14 +3,16 @@
  */
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 let app = new express();
 
-app.get('/', (req, res) => {
-  let username = req.query.username;
-   // ...
-  res.end(`username: ${username}`);
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post('/signUp', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  res.end(`username: ${username}, password: ${password}`);
 });
 
 
